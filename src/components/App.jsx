43664ca0918b20bb67703a6cd41ec4db9f5d78a4
officejake <31710782+officejake@users.noreply.github.com>
2018-03-25 16:39:54 -0400

    add form reset on submit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,83 +9,90 @@ class App extends Component {
         }
     }
 
-    addReminder() {
-        this.props.addReminder(this.state.text);
-    }
+   addReminder() {
+      this.props.addReminder(this.state.text);
+   }
 
-    deleteReminder(id) {
-       this.props.deleteReminder(id);
-    }
+   deleteReminder(id) {
+      this.props.deleteReminder(id);
+   }
 
-    renderReminders(){
-        const { reminders } = this.props;
-        return (
-            <ul className='reminderList'>
-                {
-                    reminders.map(reminder => {
-                        return (
-                            <li className='reminderItem' key={reminder.id}>
-                                <div>{reminder.text}</div>
-                                <div 
-                                    className='deleteBtn'
-                                    onClick={() => this.deleteReminder(reminder.id)}
-                                 >
-                                    &#x2715;
-                                </div>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        )
-    }
+   handleSubmit(e) {
+      e.preventDefault();
+      e.target.reset();
+   }
 
-    render() {
-        return (
-            <div className="App">
-                <div>
-                    <div className="form-group">
-                    <label>
-                        <input
-                           className="form-control"
-                           placeholder='Eviscerate the Proletariat'
-                           onChange={event => this.setState({text: event.target.value})}
-                           onKeyPress={event => {
-                              if (event.key === 'Enter') {
-                                 this.addReminder()
-                              }
-                           }}
-                        />
-                        <div className="label-text"> I have to ... </div>
-                    </label>
-                    </div>
-                    <button
-                        type="button"
-                        className="btnSubmit"
-                        onClick={() => this.addReminder()}
-                     >
-                    Add reminder
-                    </button>
-                </div>
-                { this.renderReminders() }
-                <div>
-                  <button
-                     type="button" 
-                     className='clearBtn'
-                     onClick={() => this.props.clearReminders()}
-                  >
-                  Clear All
-                  </button>
-                </div>
+   renderReminders(){
+      const { reminders } = this.props;
+      return (
+         <ul className='reminderList'>
+            {
+               reminders.map(reminder => {
+                  return (
+                     <li className='reminderItem' key={reminder.id}>
+                        <div>{reminder.text}</div>
+                           <div 
+                              className='deleteBtn'
+                              onClick={() => this.deleteReminder(reminder.id)}
+                           >
+                           &#x2715;
+                        </div>
+                     </li>
+                     )
+                  })
+               }
+         </ul>
+         )
+      }
+
+   render() {
+      return (
+         <div className="App">
+            <div>
+               <form onSubmit={this.handleSubmit.bind(this)}>
+                  <label>
+                     <input
+                        value={this.state.value}
+                        className="form-control"
+                        placeholder='Eviscerate the Proletariat'
+                        onChange={event => this.setState({text: event.target.value})}
+                        onKeyPress={event => {
+                           if (event.key === 'Enter') {
+                              this.addReminder()
+                           }
+                        }}
+                        onSubmit={this.handleChange}
+                     />
+                     <div className="label-text"> I have to ... </div>
+                  </label>
+               </form>
+               <button
+                  type="button"
+                  className="btnSubmit"
+                  onClick={() => this.addReminder()}
+               >
+                  Add reminder
+               </button>
             </div>
-        )
-    }
+            { this.renderReminders() }
+            <div>
+               <button
+                  type="button" 
+                  className='clearBtn'
+                  onClick={() => this.props.clearReminders()}
+               >
+               Clear All
+               </button>
+            </div>
+         </div>
+      )
+   }
 }
 
 function mapStateToProps(state) {
-    return {
-        reminders: state
-    }
+   return {
+      reminders: state
+   }
 }
 
 export default connect(mapStateToProps, { addReminder, deleteReminder, clearReminders })(App);
